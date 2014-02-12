9df28dfbf6d52ad65e3dd31e17afcfe2c00e2bd3
SAKATA Sinji <contact@sinjis-view.mydns.jp>
2014-02-13 01:07:49 +0900

    Add init script

---
+++ init.rb
@@ -0,0 +1,16 @@
+﻿require 'open3'
+
+Open3.capture3(gem update -system)
+
+packages = ['sinatra', 'sqlite3', 'activerecord']
+packages.each do |package|
+  print "[install] #{package}\n"
+  out, err, status = Open3.capture3("gem install #{package}")
+  if status.success? then
+    print "#{out}\n"
+  else
+    print "#{err}\nInitialization of BlogGem is not complete.\nPlease solve the problem."
+    break
+  end
+  
+end
