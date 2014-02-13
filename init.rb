fa3f05569fd78b3b227efac2035eeb144113438e
SAKATA Sinji <contact@sinjis-view.mydns.jp>
2014-02-13 16:35:33 +0900

    Init script of database
    Rename table sql file.

--- init.rb
+++ init.rb
@@ -1,16 +1,29 @@
 ﻿require 'open3'
+require 'rubygems'
+require 'sqlite3'
+include SQLite3
 
-Open3.capture3(gem update -system)
+Open3.capture3('gem update -system')
 
+#install gem packages
 packages = ['sinatra', 'sqlite3', 'activerecord']
 packages.each do |package|
   print "[install] #{package}\n"
-  out, err, status = Open3.capture3("gem install #{package}")
+  out, err, status = Open3.capture3("gem install --no-ri --no-rdoc #{package}")
   if status.success? then
     print "#{out}\n"
   else
     print "#{err}\nInitialization of BlogGem is not complete.\nPlease solve the problem."
-    break
+    raise "gem error"
   end
-  
 end
+
+#set up database
+print "[set up] database\n"
+sql_file = ['entry.sql', 'element.sql', 'comment.sql', 'category.sql']
+Database.new('page.db') do |database|
+  sql_file.each do |sql|
+    database.execute(open(sql).read)
+  end
+end
+print "[complete] database\n"
