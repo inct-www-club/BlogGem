156f16e81199361eaae3bf7d493b67c25f92a10c
SAKATA Sinji <contact@sinjis-view.mydns.jp>
2014-03-10 17:25:21 +0900

    class.rb is enbodied main.rb

--- main.rb
+++ main.rb
@@ -40,8 +40,6 @@ class BlogGem < Sinatra::Base
     return load_json( File.join(theme_path, "scheme.json") )
   end
 
-  load 'class.rb'
-
 
   helpers do
     def do_template(template, options = {}, locals = {}, &block)
@@ -395,5 +393,120 @@ class BlogGem < Sinatra::Base
       end
     end
   end
+end
 
+
+class Entry < ActiveRecord::Base
+  def date()
+    fdate = DateTime.parse("#{created_at}")
+    return fdate.strftime("%Y/%m/%d %H:%M")
+  end
+
+  def format(split_body=true)
+    formated = FormatedEntry.new
+    use_pre = false
+
+    formated.id = id
+
+    formated.title = title
+
+    if split_body == true then
+      _body = body.split('[read_more]')
+      formated.body = "#{_body[0]}".gsub(/(\r\n|\r|\n)/, '<br />')
+      formated.read_more = _body.size >= 2
+    else
+      enter = /(\r\n|\r|\n)/
+      formated.body = body.gsub('[read_more]', '').gsub(enter, '<br />')
+    end
+
+    #escape html inside the pre tag
+    pre_pattarn = /<pre(?:.|\n)*?pre>/
+    body_without_pre = formated.body.split(pre_pattarn, -1)
+    pre_tag = formated.body.scan(pre_pattarn)
+    use_pre = true if pre_tag.size > 0
+    formated.body = body_without_pre.shift
+    body_without_pre.length.times do
+      pre = pre_tag.shift
+      pre.gsub!("<br />", "\n")
+      inside_tag = pre[/>(?:.|\n)+</]
+      if inside_tag == nil then
+        inside_tag = ""
+      end
+      inside_tag = inside_tag[1, inside_tag.length - 2]
+      pre.gsub!(/>(?:.|\n)+</, ">#{Rack::Utils.escape_html(inside_tag)}<")
+      formated.body = formated.body + pre + body_without_pre.shift
+    end
+
+    _category_num = category.split(',')
+    _category_num.each do |c|
+      begin
+        _category =  Category.find(c.to_i)
+        formated.category << _category.name
+      end
+    end
+
+    formated.comment_num = comment_num
+
+    formated.created_at = date()
+
+    return [formated, use_pre]
+  end
+end
+
+class Comment < ActiveRecord::Base
+  def date()
+    fdate = DateTime.parse("#{created_at}")
+    return fdate.strftime("%Y/%m/%d %H:%M")
+  end
+
+  def format()
+    escaped_name = Rack::Utils.escape_html(name)
+    escaped_body = Rack::Utils.escape_html(body).gsub(/(\r\n|\r|\n)/,'<br />')
+    formated = FormatedComment.new(
+      id,
+      entry_id,
+      escaped_name,
+      escaped_body,
+      date()
+      )
+    return formated
+  end
+end
+
+class Category < ActiveRecord::Base
+end
+
+class Searcher < ActiveRecord::Base
+end
+
+class FormatedEntry
+  def initialize()
+    @id
+    @title
+    @body
+    @read_more = false
+    @category = Array.new
+    @comment_num
+    @created_at
+  end
+  attr_accessor(
+    :id,
+    :title,
+    :body,
+    :read_more,
+    :category,
+    :comment_num,
+    :created_at
+    )
+end
+
+class FormatedComment
+  def initialize(id, entry_id, name, body, created_at)
+    @id = id
+    @entry_id = entry_id
+    @name = name
+    @body = body
+    @created_at = created_at
+  end
+  attr_accessor :id, :entry_id, :name, :body, :created_at
 end
