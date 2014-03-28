e2a5b7ea9cc976066551aad849a7cff05975f061
SAKATA Sinji <contact@sinjis-view.mydns.jp>
2014-03-28 23:04:25 +0900

    thumbnailの画像サイズに関するバグ修正
    Windowサイズの変更時にthumbnailの画像サイズが変更されないバグを修正

--- design/WWW/js/sandabu.js
+++ design/WWW/js/sandabu.js
@@ -1,10 +1,9 @@
 resize_image();
 
-window.resize = resize_image();
+$(window).resize( function(){
+    resize_image();
+});
 
-//サイズの表示
 function resize_image() {
-    var thu = $("div.thuimg");
-    var width = thu.width();
-    thu.css("height", width*0.8);
+    thu.css("height", $("div.thuimg").width() * 0.8);
 }
