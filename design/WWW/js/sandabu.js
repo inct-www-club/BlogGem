d8dbc422ea84d9a4e9a3635a1759bbb6e0f07c34
SAKATA Sinji <contact@sinjis-view.mydns.jp>
2014-04-04 11:09:05 +0900

    画像が指定されていない場合のnoimage.png表示を実現

--- design/WWW/js/sandabu.js
+++ design/WWW/js/sandabu.js
@@ -8,3 +8,12 @@ function resize_image() {
     var thu = $('div.thuimg')
     thu.css('height', thu.width() * 0.8);
 }
+
+$(document).ready(function(){
+    $('img').error(function(){
+          $(this).attr({
+              src: '/image/noimage.png',
+              alt: 'image not founf'
+          });
+    });
+});
